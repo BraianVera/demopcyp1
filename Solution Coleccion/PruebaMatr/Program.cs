247a213d6138d5a45927cc71b8104973672c676c
Usuario <Usuario@AULA-210-11>
2019-04-09 20:30:10 -0300

    codigo landa

--- Solution Coleccion/PruebaMatr/Program.cs
+++ Solution Coleccion/PruebaMatr/Program.cs
@@ -10,19 +10,37 @@ namespace PruebaMatr
     class Program
     {
 
-        static void metodo1() { Thread.SpinWait(int.MaxValue); Console.WriteLine("metodo1");    }
-        static void metodo2() { Console.WriteLine("metodo2"); }
-        
+        static int metodo1() { Thread.SpinWait(int.MaxValue); Console.WriteLine("metodo1"); return 22; }
+        static int metodo2() { Console.WriteLine("metodo2"); return 11; }
+        static void metodo3() { Thread.SpinWait(int.MaxValue); Console.WriteLine("metodo3"); }
+        static void metodo4() { Console.WriteLine("metodo4"); }
         static void Main(string[] args)
         {
-            List<Task> tareas = new List<Task>();
-            Task t1 = Task.Factory.StartNew(metodo1);
-            tareas.Add(t1);
-            Task t2 = Task.Factory.StartNew(metodo2);
-            tareas.Add(t2);
+            //List<Task> tareas = new List<Task>();
+            Task<int> t1 = Task<int>.Factory.StartNew(metodo1);
+            //tareas.Add(t1);
+            Task t2 = t1.ContinueWith((prede) =>
+            {
+                Console.WriteLine("el resulatado  de metodo1 es:"+prede.Result);
+
+                List<Task> tareas = new List<Task>();
+                Task t3 = Task.Factory.StartNew(metodo3);
+                tareas.Add(t3);
+
+                Task t4 = Task.Factory.StartNew(metodo4);
+                tareas.Add(t4);
+
+                Task.WaitAll(tareas.ToArray());
+                //Task.WaitAny();
+
+                metodo2();
+            });
+            t2.Wait();
+            //tareas.Add(t2);
 
 
-            Task.WaitAny();
+            //Console.WriteLine("tareas posicion " + tareas[1].result(););
+            Task.WaitAll();
             Console.WriteLine("Finalizo el programa");
         }
     }
